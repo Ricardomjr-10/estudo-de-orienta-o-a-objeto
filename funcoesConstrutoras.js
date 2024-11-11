b5c196807ea0a81d79542d06c6363d43f86caab5
Ricardomjr-10 <ricardomjr2@gmail.com>
2024-11-11 16:00:05 -0300

    criando metodo estatico formateDataBase

--- funcoesConstrutoras.js
+++ funcoesConstrutoras.js
@@ -3,8 +3,12 @@
         this.name = name
         this.emal = email
     }
-} */
 
+    comment () {
+        return `${this.name} comentou`
+    }
+}
+ */
     function Studante (name, email) { // classe por baixo dos panos
         this.name = name
         this.email = email
@@ -17,12 +21,21 @@
 
     Studante.prototype.comment = function () {
         return `${this.name} comentou`
+    } 
+        
+    Studante.formatDataBase = function (aString) {
+        return aString
+        .toUpperCase()
+        .replaceAll(' ', '_')
     }
+   
 
 const gabrielFialho = new Studante('Gabriel Fialho', 'gabrielf@rogermelo.com.br')
 const brenoLemos = new Studante('Breno lemos', 'brenolemos@rogermelo.com.br')
 
 console.log(gabrielFialho.login(), brenoLemos.comment())
+console.log(gabrielFialho, brenoLemos)
+console.log(Studante.formatDataBase('string para o banco de dados'))
 
 //prototypes
 console.log([].includes())
