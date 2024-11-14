1551f42458fe89bf677d92b75e825eab4617f2ca
Ricardomjr-10 <ricardomjr2@gmail.com>
2024-11-14 15:47:25 -0300

    resolvendo exercicio

---
+++ exercicio1.js
@@ -0,0 +1,7 @@
+//  - A invocação da função abaixo deve retornar este objeto:
+//     {
+//       prop1: 'value1',
+//       prop2: 'value2',
+//       prop3: 'value3' 
+//     }
+//   - Descomente o código e crie a função.
