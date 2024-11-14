87e4361d8ed28ba462f523c90ef08ebb5e7c138a
Ricardomjr-10 <ricardomjr2@gmail.com>
2024-11-14 15:50:14 -0300

    criando a saida no console

--- exercicio1.js
+++ exercicio1.js
@@ -5,3 +5,10 @@
 //       prop3: 'value3' 
 //     }
 //   - Descomente o código e crie a função.
+
+
+console.log(convertToObj([
+  'prop1', 'value1',
+  'prop2', 'value2',
+  'prop3', 'value3'
+]))
