75877c4505ce64ab73fe8a6fa209a14ff399e7a0
Ricardomjr-10 <ricardomjr2@gmail.com>
2024-11-14 15:51:50 -0300

    renomeando funcao

--- exercicio1.js
+++ exercicio1.js
@@ -7,8 +7,10 @@
 //   - Descomente o código e crie a função.
 
 
-console.log(convertToObj([
-  'prop1', 'value1',
-  'prop2', 'value2',
-  'prop3', 'value3'
-]))
+console.log(
+  arrayToObj([
+    ['prop1', 'value1'],
+    ['prop2', 'value2'],
+    ['prop3', 'value3']
+  ])
+)
